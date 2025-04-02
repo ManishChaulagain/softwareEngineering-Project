0c4b6344e688c350f3f3135480e2df0cd1e6abb8
Manish Chaulagain <iammanish321@gmail.com>
2025-04-02 13:41:01 -0500

    fixed

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -2,7 +2,7 @@
 
 import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.STRIPE_SECRETE_KEY);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {

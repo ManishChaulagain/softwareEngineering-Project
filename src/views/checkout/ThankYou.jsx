3f34f9e7ba82bd34570d0180d2208a7eb113c2d5
Manish Chaulagain <iammanish321@gmail.com>
2025-04-02 14:12:57 -0500

    style fixed

--- src/views/checkout/ThankYou.jsx
+++ src/views/checkout/ThankYou.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
+import "./Thankyou.css"
 
 const ThankYou = () => {
   const [sessionId, setSessionId] = useState(null);

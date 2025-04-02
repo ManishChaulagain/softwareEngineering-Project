291ffbd1ab7244d29989c65391a2d1209ea815a0
Manish Chaulagain <iammanish321@gmail.com>
2025-04-02 00:51:37 -0500

    fixes

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
@@ -14,5 +12,17 @@ export default defineConfig({
   },
   server: {
     port: 3000
-  }
-})
+  },
+  // 🔥 THIS IS THE KEY PART FOR VERCE/SPAs
+  build: {
+    outDir: 'dist'
+  },
+  // 👇 Enable history fallback
+  preview: {
+    port: 4173,
+    strictPort: true
+  },
+  // 👇 Most important
+  base: '/',
+  publicDir: 'public'
+});
